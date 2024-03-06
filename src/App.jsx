bb492ca78d670cdee15eccfcef4c9e2a3656f3fc
Casey <yoshiplanet2092@gmail.com>
2024-03-07 08:40:45 +1100

    camera options added

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,39 @@ const apiKey = import.meta.env.VITE_API_KEY
 
 export default function App() {
 
-
   const [images, setImages] = useState([])
   const [camera, setCamera] = useState('FHAZ')
   const [date, setDate] = useState('2015-6-3')
   const [rover, setRover] = useState('curiosity')
 
-  const handleCamera = e => setCamera(e.target.value)
-  const handleDate = e => setDate(e.target.value)
-  const handleRover = e => setRover(e.target.value)
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&camera=${camera}&page=${'1'}&api_key=${apiKey}`)
       .then(res => res.data.photos)
       .then(setImages)
-  }, [camera])
+  }, [camera, date, rover])
 
 
+  const handleCamera = e => setCamera(e.target.value)
+  const handleDate = e => {
+    setDate(e.target.value)
+    console.log(date);
+  }
+  const handleRover = e => setRover(e.target.value)
+
+  console.log(images)
   return (
     <div className="App">
 
-      
-      <input aria-label="Date" type="date" onChange={handleDate} />
-      {images.map(image => {
-        return <img src={image.img_src} />
+      <p>Select Image Date:</p>
+      <input
+        type="date"
+        onChange={handleDate}
+        // min={images[0].rover.landing_date}
+        // max={images[0].rover.max_date}
+      />
 
-      })}
+      <p>Select Rover Camera:</p>
       <select name="camera" onChange={handleCamera}>
         <option value="FHAZ">Front Hazard Avoidance Camera</option>
         <option value="RHAZ">	Rear Hazard Avoidance Camera</option>
@@ -45,7 +52,20 @@ export default function App() {
         <option value="MINITES">Miniature Thermal Emission Spectrometer</option>
       </select>
 
-      {photos.map(data => <img src={data.img_src} alt={"This is an image of the mars " + data.rover.name + " rover."}/>)}
+      <p>Select Mars Rover:</p>
+      <select name="rover" onChange={handleRover}>
+        <option value="Curiosity">Curiosity</option>
+        <option value="Opportunity">Opportunity</option>
+        <option value="Spirit">Spirit</option>
+      </select>
+
+      {images.map(data => {
+        <img
+          src={data.img_src}
+          alt={"This is an image from the " + data.camera.full_name + " camera of the mars " + data.rover.name + " rover."}
+        />
+        console.log(data);
+      })}
 
 
     </div>
